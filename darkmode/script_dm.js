4bf6e4784c8a1358b4034af1bdd6d63e5305abba
tuguu0413 <146718055+tuguu0413@users.noreply.github.com>
2023-10-29 13:47:27 +0800

    Add files via upload

---
+++ darkmode/script_dm.js
@@ -0,0 +1,39 @@
+const dialog = document.getElementById('dialog');
+const content = document.getElementById('content');
+const close = document.getElementById('close');
+
+document.getElementById('potato').addEventListener('click', () => {
+    document.getElementById('1').style.display = 'block';
+    document.getElementById('2').style.display = 'none';
+    document.getElementById('3').style.display = 'none';
+    dialog.style.display = 'block';
+});
+
+document.getElementById('carrot').addEventListener('click', () => {
+    document.getElementById('1').style.display = 'none';
+    document.getElementById('2').style.display = 'block';
+    document.getElementById('3').style.display = 'none';
+    dialog.style.display = 'block';
+});
+
+document.getElementById('cabbage').addEventListener('click', () => {
+    document.getElementById('1').style.display = 'none';
+    document.getElementById('2').style.display = 'none';
+    document.getElementById('3').style.display = 'block';
+    dialog.style.display = 'block';
+});
+
+document.addEventListener('click', (event) => {
+    if (event.target === dialog) {
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'none';
+        document.getElementById('3').style.display = 'none';
+        dialog.style.display = 'none';
+    }
+});
+
+function toggleLightMode() {
+    window.location.href = '../main.html';
+}
+
+document.getElementById('lightModeButton').addEventListener('click', toggleLightMode);
