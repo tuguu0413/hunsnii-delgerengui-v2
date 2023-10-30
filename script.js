732235cda741161cee137e730af3968ba05865fc
tuguu0413 <146718055+tuguu0413@users.noreply.github.com>
2023-10-30 21:39:01 +0800

    comment

--- script.js
+++ script.js
@@ -51,3 +51,26 @@ function createParagraph() {
     document.getElementById("userInput").value = "";
   }
   document.getElementById("createButton").addEventListener("click", createParagraph);
+  
+  let currentSlide = 0;
+    const slides = document.querySelectorAll(".slider-slide");
+
+    function showSlide(slideIndex) {
+      if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+      } else if (slideIndex >= slides.length) {
+        slideIndex = 0;
+      }
+
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+
+      slides[slideIndex].style.display = "block";
+      currentSlide = slideIndex;
+    }
+
+    function changeSlide(n) {
+      showSlide(currentSlide + n);
+    }
+    showSlide(currentSlide);
