013d028278867182a6be2d88d74e0031eca3b544
Zadyy <nyambayar2014@gmail.com>
2023-11-05 21:31:40 +0800

    toggle dark and light mode

--- darkmode/script_dm.js
+++ darkmode/script_dm.js
@@ -32,8 +32,8 @@ document.addEventListener('click', (event) => {
     }
 });
 
-function toggleLightMode() {
-    window.location.href = '../main.html';
-}
+// function toggleLightMode() {
+//     window.location.href = '../main.html';
+// }
 
-document.getElementById('lightModeButton').addEventListener('click', toggleLightMode);
+// document.getElementById('lightModeButton').addEventListener('click', toggleLightMode);
